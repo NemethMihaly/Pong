dfcaf8543c7a3f4cfb3d2f4ed47bd3f7b72daaab
NemethMihaly <nemeth.mihaly202@gmail.com>
2025-07-19 19:15:51 +0200

    Add Win32-window creation and message loop

--- Source/Pong/Pong.cpp
+++ Source/Pong/Pong.cpp
@@ -2,7 +2,51 @@
 
 #pragma warning(disable: 28251) // Disable warning about inconsistent SAL annotations
 
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
 {
-	return MessageBox(nullptr, L"Hello, World!", L" ", MB_OK);
+    WNDCLASS wc = {};
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = L"PongWindowClass";
+    if (!RegisterClass(&wc))
+        return 0;
+
+    RECT rc = { 0, 0, 640, 480 };
+    AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, FALSE);
+
+    HWND hWnd = CreateWindow(L"PongWindowClass", L"Pong", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 
+        rc.right - rc.left, rc.bottom - rc.top, nullptr, nullptr, hInstance, nullptr);
+    if (!hWnd)
+        return 0;
+
+    ShowWindow(hWnd, SW_SHOW);
+
+    MSG msg = {};
+    while (msg.message != WM_QUIT)
+    {
+        if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return 0;
+}
+
+LRESULT WindowProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    switch (msg)
+    {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    
+    default:
+        return DefWindowProc(hWnd, msg, wParam, lParam);
+    }
+
+    return 0;
 }
