e8d1a73cc5ee4f30408d6c49ae5b491805aea7fa
NemethMihaly <nemeth.mihaly202@gmail.com>
2025-07-21 11:35:53 +0200

    Add win32-window creation and run loop

--- Source/Pong/Pong.cpp
+++ Source/Pong/Pong.cpp
@@ -4,9 +4,75 @@
 
 #pragma warning(disable: 28251) // Disable warning about inconsistent SAL annotations
 
+LRESULT CALLBACK MsgProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
 {
-	LOG("Main", Verbose, "Hello, World! %i %f %s", 1, 2.3f, "4");
+    WNDCLASS wc;
+    wc.style = CS_HREDRAW | CS_VREDRAW;
+    wc.lpfnWndProc = MsgProc;
+    wc.cbClsExtra = 0;
+    wc.cbWndExtra = 0;
+    wc.hInstance = hInstance;
+    wc.hIcon = LoadIcon(nullptr, IDI_APPLICATION);
+    wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+    wc.lpszMenuName = nullptr;
+    wc.lpszClassName = L"PongWindowClass";
+    if (!RegisterClass(&wc))
+    {
+        return 0;
+    }
+
+    RECT rc;
+    SetRect(&rc, 0, 0, 640, 480);
+    AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, FALSE);
+
+    HWND hwnd = CreateWindow(L"PongWindowClass", L"Pong", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT,
+        rc.right - rc.left, rc.bottom - rc.top, nullptr, nullptr, hInstance, nullptr);
+    if (hwnd == nullptr)
+    {
+        return 0;
+    }
+
+    if (!IsWindowVisible(hwnd))
+        ShowWindow(hwnd, SW_SHOW);
+
+    MSG msg;
+    ZeroMemory(&msg, sizeof(MSG));
+    while (msg.message != WM_QUIT)
+    {
+        if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
 
 	return 0;
+}
+
+LRESULT MsgProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    switch (msg)
+    {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+
+    case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hwnd, &ps);
+
+            EndPaint(hwnd, &ps);
+
+            break;
+        }
+
+    default:
+        return DefWindowProc(hwnd, msg, wParam, lParam);
+    }
+
+    return 0;
 }
